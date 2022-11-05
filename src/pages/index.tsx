1ff640b1673438d5948422e212a2137c9c4159e7
Reid Levesque <reidlevesque@users.noreply.github.com>
2022-11-05 11:37:10 -0400

    Add demographics page and thank you page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,14 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import React from "react";
-import Table from "../@shared/table/Table";
 
-const Instructions: NextPage = () => {
+const Index: NextPage = () => {
   function begin() {
     // TODO: add entry in database
     window.location.href='test?id=1';
   }
 
   return (
-    <div className="instructions">
+    <div className="index">
       <h1>Allie&apos;s Science Fair</h1>
       <p>Welcome to my science fair. Details go here.</p>
       <input type="button" onClick={begin} value="Begin" />
@@ -20,4 +16,4 @@ const Instructions: NextPage = () => {
   );
 };
 
-export default Instructions;
+export default Index;
---
+++ src/pages/thanks.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import React from "react";
+
+const Thanks: NextPage = () => {
+  return (
+    <div className="thanks">
+      <h1>Allie&apos;s Science Fair</h1>
+      <p>Thank you for helping me gather data for my science fair.</p>
+    </div>
+  );
+};
+
+export default Thanks;
