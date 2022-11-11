cbc430bc4f10ad229d20ebcc5d414d194f6b5c14
Allie Levesque <allielevesque@users.noreply.github.com>
2022-11-11 12:31:47 -0500

    Test 4 done

--- src/@shared/tests/TestData.ts
+++ src/@shared/tests/TestData.ts
@@ -52,4 +52,21 @@ export const testData: TestDatum[] = [
 
     ],
   },
+  // Test 4
+  {
+    instructions: " Select the row that is inbetween two rows that have a X inbetween a green X and a purple X ",
+    correctRow: 5,
+    colours: [
+      ["red","purple","black","green","black","green","red","black","purple","black","red","purple","black","green",],
+      ["green","red","green","red","black","purle","black","red","green","black","red","red","black","green",],
+      ["red","black","green","green","red","black","purple","black","red","black","green","black","purple","red",],
+      ["black","red","green","black","black","green","red","purple","green","black","red","red","purple","red",],
+      ["green","black","red","black","purple","red","black","red","red","black","red","purple","green","black",],
+      ["black","red","green","red","black","black","purple","black","purple","red","green","black","green","black",],
+      ["red","black","green","red","purple","black","black","red","black","red","black","green","black","red",],
+      ["purple","black","green","red","black","red","purple","black","green","red","black","purple","red","black",],
+      ["black","red","green","black","red","purple","black","green","black","black","red","green","purple","black",],
+
+    ],
+  },
 ]
