969daf0e4bed11cd1401e325f1c92bae42f78f7f
Allie Levesque <allielevesque@users.noreply.github.com>
2022-11-20 14:06:34 -0500

    Add colours to the instructions

--- src/@shared/tests/TestData.ts
+++ src/@shared/tests/TestData.ts
@@ -4,68 +4,96 @@ export type TestDatum = {
   colours: string[][];
 };
 
+var black_ = "#000000";
+var red___ = "#ff0000";
+var green_ = "green";
+var purple = "purple";
+var pink__ = "#eb13b5";
+var orange = "#d45a08";
+
+function x(colour: string): string {
+  return "<span style=\"color: " + colour + "\">X</span>"
+}
+
 export const testData: TestDatum[] = [
   // Test 1
   {
-    instructions: " Select the row with the red X.",
+    instructions: " Select the row with the red " + x(red___),
     correctRow: 2,
     colours: [
-      ["black","black","black","black","black","black","black","black","black"],
-      ["black","black","black","black","black","black","red","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_],
+      [black_, black_, black_, black_, black_, black_, red___, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
     ],
   },
   // Test 2
   {
-    instructions: " Select the row with the red X ",
+    instructions: " Select the row with the red " + x(red___),
     correctRow: 0,
     colours: [
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","black","black","black","black","black",],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, black_, black_, black_, black_, black_,],
     ],
   },
   // Test 3
   {
-    instructions: " Select the row with three X's ",
+    instructions: " Select the row with three red " + x(red___) + "'s ",
     correctRow: 6,
     colours: [
-      ["red","black","black","red","black","black","red","black","black","black","red","black","black","black",],
-      ["black","red","black","red","black","black","black","red","black","black","red","black","black","black",],
-      ["black","black","black","black","red","black","black","black","red","black","black","black","black","black",],
-      ["black","red","black","black","black","black","red","black","black","black","black","red","black","red",],
-      ["black","black","red","black","black","red","black","red","black","black","red","black","black","black",],
-      ["black","red","black","red","black","black","black","black","black","red","black","black","black","black",],
-      ["red","black","black","red","black","black","black","red","black","red","black","black","black","red",],
-      ["black","black","black","black","black","red","black","black","black","black","black","black","black","black",],
-      ["black","black","black","black","red","black","black","black","black","black","black","black","red","black",],
+      [red___, black_, black_, red___, black_, black_, red___, black_, black_, black_, red___, black_, black_, black_,],
+      [black_, red___, black_, red___, black_, black_, black_, red___, black_, black_, red___, black_, black_, black_,],
+      [black_, black_, black_, black_, red___, black_, black_, black_, red___, black_, black_, black_, black_, black_,],
+      [black_, red___, black_, black_, black_, black_, red___, black_, black_, black_, black_, red___, black_, red___,],
+      [black_, black_, red___, black_, black_, red___, black_, red___, black_, black_, red___, black_, black_, black_,],
+      [black_, red___, black_, red___, black_, black_, black_, black_, black_, red___, black_, black_, black_, black_,],
+      [red___, black_, black_, red___, black_, black_, black_, red___, black_, red___, black_, black_, black_, red___,],
+      [black_, black_, black_, black_, black_, red___, black_, black_, black_, black_, black_, black_, black_, black_,],
+      [black_, black_, black_, black_, red___, black_, black_, black_, black_, black_, black_, black_, red___, black_,],
 
     ],
   },
   // Test 4
   {
-    instructions: " Select the row that is inbetween two rows that have a X inbetween a green X and a purple X ",
+    instructions: " Select the row with a red " + x(red___) + " in between a pink " + x(pink__) + " and orange " + x(orange),
+    correctRow: 0,
+    colours: [
+      [red___, pink__, black_, red___, orange, pink__, red___, black_, black_, green_, red___, black_, black_, pink__, red___, green_, black_, black_, orange, orange, black_, black_, pink__, black_, orange, black_, black_, red___, red___, green_, black_, pink__, black_, green_, black_,],
+      [green_, red___, pink__, black_, black_, pink__, black_, orange, orange, pink__, black_, green_, black_, red___, black_, black_, red___, orange, black_, black_, green_, pink__, green_, black_, red___, red___, black_, pink__, black_, black_, orange, green_, black_, pink__, black_,],
+      [red___, red___, black_, black_, green_, pink__, orange, black_, red___, pink__, black_, black_, orange, orange, pink__, black_, green_, black_, black_, red___, red___, black_, green_, orange, black_, black_, pink__, green_, black_, black_, orange, black_, black_, green_, black_,],
+      [black_, red___, black_, black_, green_, green_, green_, black_, pink__, red___, black_, red___, orange, red___, black_, pink__, black_, black_, black_, green_, red___, black_, black_, pink__, green_, black_, orange, pink__, orange, black_, green_, pink__, black_, green_, black_,],
+      [pink__, black_, red___, green_, orange, red___, black_, pink__, pink__, black_, red___, orange, red___, red___, black_, green_, black_, orange, pink__, red___, black_, green_, red___, black_, black_, green_, pink__, pink__, black_, black_, orange, orange, black_, green_, black_,],
+      [orange, red___, black_, red___, pink__, pink__, black_, green_, orange, red___, black_, pink__, green_, pink__, black_, black_, green_, black_, red___, orange, pink__, black_, green_, green_, black_, pink__, black_, green_, black_, orange, orange, red___, black_, black_, pink__,],
+      [red___, black_, pink__, red___, green_, green_, black_, red___, orange, red___, red___, black_, black_, pink__, black_, red___, orange, black_, green_, pink__, red___, black_, black_, black_, orange, red___, black_, green_, green_, pink__, pink__, black_, green_, black_, orange,],
+      [black_, red___, pink__, black_, green_, red___, orange, green_, pink__, black_, black_, red___, pink__, black_, black_, orange, red___, red___, black_, black_, green_, black_, orange, pink__, black_, green_, black_, red___, black_, pink__, red___, green_, green_, black_, black_,],
+      [green_, black_, black_, red___, red___, pink__, black_, orange, green_, green_, black_, pink__, red___, black_, orange, orange, red___, black_, black_, pink__, red___, green_, black_, orange, green_, black_, black_, red___, black_, pink__, green_, black_, orange, green_, pink__,],
+
+    ],
+  },
+  // Test 5
+  {
+    instructions: " Select the row that is in between two rows that have a red " + x(red___) + " in between a green " + x(green_) + " and a purple " + x(purple),
     correctRow: 5,
     colours: [
-      ["red","purple","black","green","black","green","red","black","purple","black","red","purple","black","green",],
-      ["green","red","green","red","black","purle","black","red","green","black","red","red","black","green",],
-      ["red","black","green","green","red","black","purple","black","red","black","green","black","purple","red",],
-      ["black","red","green","black","black","green","red","purple","green","black","red","red","purple","red",],
-      ["green","black","red","black","purple","red","black","red","red","black","red","purple","green","black",],
-      ["black","red","green","red","black","black","purple","black","purple","red","green","black","green","black",],
-      ["red","black","green","red","purple","black","black","red","black","red","black","green","black","red",],
-      ["purple","black","green","red","black","red","purple","black","green","red","black","purple","red","black",],
-      ["black","red","green","black","red","purple","black","green","black","black","red","green","purple","black",],
+      [red___, purple, black_, green_, black_, green_, red___, black_, purple, black_, red___, purple, black_, green_,],
+      [green_, red___, green_, red___, black_, purple, black_, red___, green_, black_, red___, red___, black_, green_,],
+      [red___, black_, green_, green_, red___, black_, purple, black_, red___, black_, green_, black_, purple, red___,],
+      [black_, red___, green_, black_, black_, green_, red___, purple, green_, black_, red___, red___, purple, red___,],
+      [green_, black_, red___, black_, purple, red___, black_, red___, red___, black_, red___, purple, green_, black_,],
+      [black_, red___, green_, red___, black_, black_, purple, black_, purple, red___, green_, black_, green_, black_,],
+      [red___, black_, green_, red___, purple, black_, black_, red___, black_, red___, black_, green_, black_, red___,],
+      [purple, black_, green_, red___, black_, red___, purple, black_, green_, red___, black_, purple, red___, black_,],
+      [black_, red___, green_, black_, red___, purple, black_, green_, black_, black_, red___, green_, purple, black_,],
 
     ],
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ const Index: NextPage<Props> = (props) => {
     <div className={styles.main}>
       <h1>Allie&apos;s Science Fair</h1>
       <p>Welcome to my science fair. My science is about why people see things and why they don't.
-         Also it is about what effects that. Before each test you will be prestioned with intrucktions
+        Also it is about what effects that. Before each test you will be prestioned with intructions
         read them then click the button that says "start test" to start. </p>
       <input className={styles.button} type="button" onClick={begin} value="Begin" />
     </div>
