a8923c214f514560474f83c37a5377a6989c11cd
Allie Levesque <allielevesque@users.noreply.github.com>
2022-11-26 11:06:37 -0500

    6,7,8,9,10

--- src/@shared/tests/TestData.ts
+++ src/@shared/tests/TestData.ts
@@ -97,4 +97,89 @@ export const testData: TestDatum[] = [
 
     ],
   },
+  // Test 6
+  {
+    instructions: " Select the row that has no red " + x(red___),
+    correctRow: 3,
+    colours: [
+      [purple, red___, black_, green_, pink__, green_, orange, black_, black_, green_, orange, purple, pink__, green_,],
+      [green_, red___, orange, red___, black_, purple, black_, orange, pink__, black_, purple, red___, black_, purple,],
+      [purple, black_, green_, orange, black_, black_, purple, black_, pink__, black_, black_, orange, purple, green_,],
+      [black_, orange, red___, pink__, black_, green_, purple, purple, green_, black_, red___, pink__, purple, orange,],
+      [orange, black_, pink__, black_, purple, red___, black_, orange, pink__, black_, red___, purple, green_, black_,],
+      [black_, red___, orange, red___, black_, black_, purple, black_, orange, red___, green_, pink__, green_, black_,],
+      [orange, black_, pink__, red___, purple, black_, black_, pink__, black_, pink__, black_, green_, orange, red___,],
+      [purple, black_, pink__, red___, black_, red___, purple, black_, green_, orange, black_, purple, red___, black_,],
+      [orange, pink__, green_, black_, red___, purple, black_, green_, black_, black_, red___, pink__, purple, black_,],
+
+    ],
+  },
+  // Test 7
+  {
+    instructions: " Select the row has 2 red " + x(red___) + "s 4 green " + x(green_) + "s and 1 pink " + x(pink__),
+    correctRow: 8,
+    colours: [
+      [red___, purple, orange, green_, black_, pink__, red___, black_, purple, black_, pink__, purple, black_, green_,],
+      [green_, pink__, green_, red___, black_, purple, black_, pink__, green_, black_, red___, orange, black_, green_,],
+      [red___, black_, green_, orange, red___, black_, purple, black_, pink__, black_, green_, pink__, purple, orange,],
+      [black_, red___, green_, black_, black_, orange, red___, purple, green_, black_, red___, pink__, black_, orange,],
+      [green_, black_, red___, black_, purple, orange, black_, pink__, orange, black_, red___, purple, pink__, black_,],
+      [pink__, red___, green_, red___, orange, black_, purple, pink__, purple, red___, green_, black_, green_, black_,],
+      [orange, black_, green_, orange, purple, black_, pink__, red___, black_, red___, black_, green_, green_, red___,],
+      [green_, black_, pink__, green_, black_, red___, purple, black_, green_, orange, black_, green_, red___, black_,],
+      [black_, red___, orange, pink__, red___, purple, black_, green_, black_, pink__, red___, green_, purple, orange,],
+
+    ],
+  },
+  // Test 8
+  {
+    instructions: " Select the row that is in between two rows that have a red " + x(red___) + " in between a green " + x(green_) + " and a purple " + x(purple),
+    correctRow: 6,
+    colours: [
+      [red___, purple, black_, green_, orange, pink__, red___, black_, pink__, black_, red___, purple, orange, green_,],
+      [green_, red___, pink__, red___, black_, orange, black_, red___, pink__, black_, red___, purple, black_, green_,],
+      [red___, black_, orange, green_, red___, black_, pink__, black_, red___, purple, green_, black_, purple, red___,],
+      [black_, red___, orange, black_, pink__, green_, red___, purple, green_, black_, pink__, red___, purple, red___,],
+      [green_, black_, red___, orange, purple, red___, black_, pink__, red___, orange, red___, purple, green_, black_,],
+      [black_, red___, orange, pink__, green_, black_, purple, black_, purple, red___, orange, green_, purple, black_,],
+      [red___, black_, green_, red___, orange, black_, black_, pink__, green_, pink__, black_, green_, black_, red___,],
+      [purple, black_, green_, orange, black_, red___, purple, pink__, green_, red___, black_, purple, red___, black_,],
+      [black_, red___, green_, pink__, red___, purple, black_, green_, orange, black_, red___, orange, purple, black_,],
+
+    ],
+  },
+  // Test 9
+  {
+    instructions: " Select the row that is in between two rows that have a red " + x(red___) + " in between a green " + x(green_) + " and a purple " + x(purple),
+    correctRow: 0,
+    colours: [
+      [orange, purple, black_, pink__, black_, green_, red___, orange, purple, black_, red___, purple, black_, green_,],
+      [pink__, red___, orange, red___, black_, purple, black_, red___, orange, black_, red___, pink__, black_, green_,],
+      [red___, black_, green_, orange, red___, black_, purple, pink__, red___, black_, green_, black_, pink__, red___,],
+      [black_, orange, green_, black_, black_, pink__, red___, purple, green_, orange, red___, red___, purple, red___,],
+      [pink__, black_, red___, orange, purple, red___, black_, red___, red___, pink__, red___, purple, green_, black_,],
+      [black_, red___, orange, red___, black_, black_, purple, black_, purple, red___, pink__, black_, green_, black_,],
+      [red___, black_, green_, pink__, purple, black_, black_, red___, black_, orange, black_, green_, pink__, red___,],
+      [purple, black_, orange, red___, black_, red___, purple, pink__, green_, red___, black_, purple, pink__, black_,],
+      [black_, red___, green_, orange, red___, purple, black_, pink__, black_, black_, red___, orange, purple, black_,],
+
+    ],
+  },
+  // Test 10
+  {
+    instructions: " Select the row that is in between two rows that have a red " + x(red___) + " in between a green " + x(green_) + " and a purple " + x(purple),
+    correctRow: 2,
+    colours: [
+      [red___, pink__, black_, green_, black_, green_, red___, black_, purple, orange, red___, purple, black_, orange,],
+      [pink__, red___, green_, red___, orange, purple, black_, red___, pink__, black_, red___, orange, black_, green_,],
+      [red___, pink__, green_, green_, red___, black_, purple, pink__, orange, black_, green_, black_, pink__, red___,],
+      [black_, pink__, green_, black_, black_, green_, orange, purple, green_, black_, red___, red___, pink__, red___,],
+      [green_, black_, pink__, black_, purple, red___, black_, orange, red___, black_, red___, pink__, green_, red___,],
+      [black_, red___, pink__, red___, black_, orange, purple, black_, purple, red___, pink__, black_, green_, black_,],
+      [orange, black_, green_, pink__, purple, black_, black_, pink__, black_, red___, black_, green_, black_, orange,],
+      [purple, pink__, green_, red___, black_, orange, purple, black_, orange, red___, orange, purple, pink__, black_,],
+      [pink__, red___, green_, black_, red___, purple, black_, green_, pink__, black_, red___, green_, orange, black_,],
+
+    ],
+  },
 ]
